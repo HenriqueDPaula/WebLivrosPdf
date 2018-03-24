061f3b1470dc587bd66e1df0281308cfd4fd45e6
opet <opet@Academico.edu>
2018-03-24 09:15:51 -0300

    Concluido

---
+++ Front/Contexts/ECommerceDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Front.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Front.Contexts
+{
+    public class ECommerceDbContext :DbContext
+    {
+        #region Properties
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Sell> Sells { get; set; }
+        public DbSet<SellItem> SellItems { get; set; }
+
+        #endregion Properties
+
+        public ECommerceDbContext()
+            : base("Asp_Net_MVC_CS")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // codigo para e
+            /* modelBuilder.Entity<Sell>()
+                        .HasOptional(s => s.SellItems)
+                        .WithOptionalDependent()
+                        .WillCascadeOnDelete(true); */
+                        
+        }
+    }
+}
